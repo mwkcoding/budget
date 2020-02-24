41c9b7190ad0e63d5c8b287be797c5ea6f682cdb
Mikkel <16936030+mwkcoding@users.noreply.github.com>
2020-02-24 22:24:39 +0100

    Added fix for MySQL error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,12 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
 use Auth;
 
 class AppServiceProvider extends ServiceProvider {
     public function boot() {
+		Schema::defaultStringLength(191);
         view()->composer('*', function($view) {
             $view->with([
                 'userName' => Auth::check() ? Auth::user()->name : null,
